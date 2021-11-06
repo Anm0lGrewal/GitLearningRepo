c8bc8cecf22e98f597747e58b23531538e70aa34
Anmol Grewal <anmolgrewal.p@gmail.com>
2021-11-06 20:54:16 +0530

    demo of git is easy

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args){
+       
+        for(int counter =0; counter<11; counter++){
+
+            System.out.println("printing hello world 10 times");
+        }
+}
+
+}

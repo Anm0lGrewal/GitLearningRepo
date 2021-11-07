fa13f822bb9ea14e8f1bcce3f8d4020d15201b9b
Anmol Grewal <anmolgrewal.p@gmail.com>
2021-11-07 09:08:20 +0530

    added new sysout in forloops

--- Demo.java
+++ Demo.java
@@ -1,10 +1,12 @@
-public class Loops {
+public class Demo {
     public static void main(String[] args){
        
         for(int counter =0; counter<11; counter++){
 
             System.out.println("printing hello world 10 times");
         }
-}
+
+        System.out.println("I like using git");
+    }
 
 }
